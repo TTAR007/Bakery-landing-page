5c02ccded465fcb85a35c3c85eec10786a087790
TTAR007 <tar127290@gmail.com>
2025-07-02 00:30:02 +0700

    finished addCart() function

--- app.js
+++ app.js
@@ -37,10 +37,66 @@ cookies.addEventListener('click', () => {
     cookiesTxt.classList.remove('undisplay-cookies-name')
 })
 
-//cart
+
+//Add food to cart
+
+//menu array
+const foodArray = [
+    {id: 1, menuName: "Matcha Cream Bun", price: 3.59, quantity: 0},
+    {id: 2, menuName: "Red Bean Bun", price: 3.59, quantity: 0}
+]
+
+let cartArray = [];
+let cartSize = 0;
+
 const cartNum = document.getElementById("cart-number");
 let count = 0;
 
-function addCart() {
+function addCart(food) {
     cartNum.textContent = ++count;
+
+    let myNewFood = food.getAttribute("data-name"); //get that name of the menu from html button
+
+    let found = false;
+    let foodIndex; // use for indicate where to add quantity
+
+    for (let i = 0; i < foodArray.length; i++) {
+        // current food that is in our store.
+        let currentFood = foodArray[i];
+
+        if (currentFood.menuName === myNewFood) {
+            //can add
+
+            // if size == 0
+            if (cartSize === 0) {
+                currentFood.quantity++;
+                cartArray[cartArray.length] = currentFood;
+                cartSize++;
+                console.log("add new food");
+                break;
+            }
+
+            // if size is not 0
+            //before adding, we have to check Is it already store in my cart?
+            for (let j = 0; j < cartArray.length; j++) {
+                if (cartArray[j].menuName === myNewFood) {
+                    found = true;
+                    foodIndex = j; // this is tne index of food that we are going to increase quantity.
+                    break;
+                }
+            }
+
+            if (found) { // increase just quantity
+                cartArray[foodIndex].quantity++;
+                console.log("increase quantity");
+                break;
+            } else { //add new food to cartArray
+                currentFood.quantity += 1;
+                cartArray[cartArray.length] = currentFood;
+                cartSize++;
+                console.log("add new food");
+                break;
+            }
+        }
+    }
 }
